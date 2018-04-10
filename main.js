ef18890ba977a96e8a1b8471c1f3610bf9bc535a
Terexit <teresamarbancastro@gmail.com>
2018-04-10 16:22:14 +0200

    buttons functions

--- main.js
+++ main.js
@@ -12,25 +12,36 @@ placeDayName.innerHTML = days[createDate.getDay()];
 var placeMonthNAme = document.querySelector('.date__month-year');
 placeMonthNAme.innerHTML = months[createDate.getMonth()] + ', ' + createDate.getFullYear();
 
-//Tasks list
-var tasks = '';
-for (var i = 0; i < tasks.length; i++) {
-  let item =
-  `<li class="tasks__item">
-    <input class="item__checkbox" type="checkbox">
-    <h4 class="item__name">${tasks[i].name}</h4>
-  </li>
-  `
-var placeTask = document.querySelector('.tasks__list');
-      // analiza la cadena de texto e inserta los nodos en el orden determinado
-placeTask.insertAdjacentHTML('beforeend', item);
-}
+// //Tasks list
+// var tasks = '';
+// for (var i = 0; i < tasks.length; i++) {
+//   let item =
+//   `<li class="tasks__item">
+//     <input class="item__checkbox" type="checkbox">
+//     <h4 class="item__name">${tasks[i].name}</h4>
+//   </li>
+//   `
+// var placeTask = document.querySelector('.tasks__list');
+//       // analiza la cadena de texto e inserta los nodos en el orden determinado
+// placeTask.insertAdjacentHTML('beforeend', item);
+// }
 
 
-//Add task
+//Add task display
 var placeShow = document.querySelector('.add__task');
 var buttonShow = document.querySelector('.button__add');
 function addTask(){
   placeShow.classList.add('show');
 }
 buttonShow.addEventListener('click', addTask);
+
+
+//Create New Task
+var allTasks = [];
+var buttonTask = document.querySelector('.new__task--button');
+function addNewTask(){
+  var placeTask = document.querySelector('.item__name');
+  var dataTask = document.querySelector('.new__task--input').value;
+  placeTask.innerHTML = dataTask;
+}
+buttonTask.addEventListener('click', addNewTask);
