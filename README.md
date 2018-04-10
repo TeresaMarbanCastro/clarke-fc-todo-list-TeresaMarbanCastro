acb1d89584285b19bba9d0eb223af10701f4288e
Terexit <teresamarbancastro@gmail.com>
2018-04-10 13:55:45 +0200

    add task

---
+++ README.md
@@ -0,0 +1,22 @@
+Desarrollo de una app simple para gestionar tareas.
+
+Funcionalidad:
+- Completar una tarea
+- Desmarcar una tarea completada
+- Añadir una tarea nueva
+
+Especificaciones
+JS nativo sin ayuda de frameworks o librerías externas
+La lista de tareas se tiene que almacenar en el Local Storage del navegador, de manera que si recargamos la página no perdamos la información
+Tiene que aparecer la fecha actual ;)
+Todo se desarrolla en la misma página, solo hay 1.
+
+Tareas:
+
+1.Completar una tarea: Hacer clic en una tarea, o en su checkbox, marca la tarea como completada y la coloca al final de la lista
+
+
+2.Desmarcar una tarea completada: Hacer clic en una tarea completada, o en su checkbox, marca la tarea como pendiente y la coloca al principio de la lista
+
+
+3.Añadir una tarea nueva: muestra una capa donde podremos rellenar el nombre de la tarea y añadirla. Aparecerá en primer lugar.
--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 'use strict'
+//DATE
+var createDate = new Date();
+var dayNumber = createDate.getDate();
+var days = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
+var months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubrer","Noviembre","Diciembre"];
+
+var placeDayNumber = document.querySelector('.date__number');
+placeDayNumber.innerHTML = dayNumber;
+var placeDayName = document.querySelector('.date__day');
+placeDayName.innerHTML = days[createDate.getDay()];
+var placeMonthNAme = document.querySelector('.date__month-year');
+placeMonthNAme.innerHTML = months[createDate.getMonth()] + ', ' + createDate.getFullYear();
+
+//Tasks list
+var tasks = '';
+for (var i = 0; i < tasks.length; i++) {
+  let item =
+  `<li class="tasks__item">
+    <input class="item__checkbox" type="checkbox">
+    <h4 class="item__name">${tasks[i].name}</h4>
+  </li>
+  `
+var placeTask = document.querySelector('.tasks__list');
+      // analiza la cadena de texto e inserta los nodos en el orden determinado
+placeTask.insertAdjacentHTML('beforeend', item);
+}
+
+
+//Add task
+var placeShow = document.querySelector('.add__task');
+var buttonShow = document.querySelector('.button__add');
+function addTask(){
+  placeShow.classList.add('show');
+}
+buttonShow.addEventListener('click', addTask);
